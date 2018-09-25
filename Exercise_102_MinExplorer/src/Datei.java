6d5286aa22dd32a6d1e2778950c1affb60ad840e
mihdoc16 <nikboyhd@gmail.com>
2018-09-25 10:28:36 +0200

    Bisschen geadded

--- Exercise_102_MinExplorer/src/Datei.java
+++ Exercise_102_MinExplorer/src/Datei.java
@@ -1,3 +1,6 @@
+
+import java.io.File;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,21 @@
  *
  * @author Dominik
  */
-public class Datei {
+public class Datei extends File{
     
+    File f;
+    
+    public Datei(String string) {
+        super(string);
+    }
+    
+    @Override
+    public String toString(){
+        if(f.isDirectory()){
+            return String.format("%s",f.getName());
+        }
+        else{
+            return String.format("%s %o %o %s",f.getName(),f.lastModified(),f.getTotalSpace());
+        }
+    }
 }
